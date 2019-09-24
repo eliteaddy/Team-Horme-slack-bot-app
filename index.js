5f5642845760ea75fa5d5db326dafe5c86424bf9
Haryormeedey <adesileisaiah70@gmail.com>
2019-09-24 09:22:55 +0100

    added slackbotjs

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const Slackbot = require("slackbots");
-const mongoose = require("mongoose");
-const axios = require("axios");
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-const token = "xoxb-771591631143-769745293696-N86Xn1K2XaI3WFm8FaTfuzmv";
+const token = "xoxb-771591631143-769745293696-JZv34J29U0Uhp6GaeTWbccUq";
 
 const bot = new Slackbot({
   token,
@@ -13,10 +11,8 @@ const bot = new Slackbot({
 // Start Handler
 bot.on("start", () => {
   bot.postMessageToChannel("general", "Lets help you save your conversation");
-  bot.postMessageToChannel(
-    "general",
-    "Sign Up or Sign in to your account by tagging me and pressing Signup or Signin"
-  );
+  bot.postMessage("general", "Welcome to Conversation Saver");
+  // console.log(res);
 });
 
 // Error Handler
@@ -24,59 +20,53 @@ bot.on("error", err => console.log(err));
 
 // Message Handler
 bot.on("message", data => {
-  if (data.type !== "message") {
-    return;
-  }
   if (data.user == bot.id) {
     return;
   }
-  console.log(data);
-  handleMessage(data);
+  if (data.type == "message") {
+    handleMessage(data);
+    console.log(data);
+  }
+  if (data.type == "app_mention") {
+    console.log("object");
+  }
+  if (data.type == "app_home") {
+    console.log("app home opened");
+  }
+  // console.log(data);
 });
 
 const handleMessage = data => {
-  var message = data.text.toLowerCase();
+  var message = data.text;
+  var channel = data.channel;
+  // console.log(channel);
   if (
     message.includes(" signup") ||
     message.includes(" sign-up") ||
     message.includes(" sign up")
   ) {
-    bot.postMessageToChannel("general", "Enter your username");
+    bot.postMessage(channel, "Enter your username");
+    // bot.postMessageToChannel("general", "Enter your username");
   }
   if (
     message.includes(" signin") ||
     message.includes(" sign-in") ||
     message.includes(" sign in")
   ) {
-    bot.postMessageToChannel("general", "Enter your username");
+    bot.postMessage(channel, "Enter your username");
+    // bot.postMessageToChannel("general", "Enter your username");
   }
   if (message.includes(" history")) {
-    const channelId = bot.getChannelId("general");
-    bot.postMessageToChannel("general", "Getting History");
+    // bot.postMessageToChannel("general", "Getting History");
+    bot.postMessage(channel, "Getting history");
 
-    getChannelHistory("channel", data.channel, data.team);
-
-    // console.log(channelId);
-    // axios
-    //   .get(
-    //     "https://slack.com/api/channels.history",
-    //     {
-    //       token: "xoxb-771591631143-769745293696-N86Xn1K2XaI3WFm8FaTfuzmv"
-    //     },
-    //     res => {
-    //       console.log(res);
-    //       bot.postMessageToChannel("general", "Done getting it");
-    //     }
-    //   )
-    //   .catch(err => {
-    //     console.log(err);
-    //     bot.postMessageToChannel("general", `Error ${err} while getting it`);
-    //   });
+    getChannelHistory(data.channel);
   }
 };
 
-const getChannelHistory = (family, value, team) => {
-  var url = `https://${team}.slack.com/api/${family}.history?token=${token}&channel=${value}`;
+const getChannelHistory = channel => {
+  var tokens = `xoxp-771591631143-769737406181-771950709478-5dfdd0b8f3fd4e748c8892cda41e2c1a`;
+  var url = `https://slack.com/api/conversations.history?token=${tokens}&channel=${channel}`;
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) console.log(xhr.responseText);
